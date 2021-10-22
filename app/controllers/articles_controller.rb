0496ceeb1a0c03a89a1e9994fbf6550c80279bff
Nicholas Jarretta <nicholas@jarretta.com>
2021-10-22 10:11:43 -0300

    add article creation

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,5 +3,22 @@ class ArticlesController < ApplicationController
   end
 
   def new
+    @article = Article.new
   end
+
+  def create
+    @article = Article.new article_params
+    if @article.save
+      flash[:success] = 'Created article'
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def article_params
+      params.require(:article).permit :title, :body
+    end
 end
---
+++ test/integration/article_creation_test.rb
@@ -0,0 +1,70 @@
+require "test_helper"
+
+class ArticleCreationTest < ActionDispatch::IntegrationTest
+  test "should work for valid articles" do
+    get new_article_path
+    assert_response :success
+
+    assert_difference 'Article.count', 1 do
+      post articles_path, params: { article: { title: 'Title',
+                                                body: 'body' } }
+    end
+
+    follow_redirect!
+
+    assert_template 'articles/index'
+    assert_response :success
+
+    assert flash[:success]
+    assert_select 'div.alert', count: 1
+  end
+
+  test "should fail for invalid articles and show errors" do
+    get new_article_path
+    assert_response :success
+
+    assert_no_difference 'Article.count', 0 do
+      post articles_path, params: { article: { title: '',
+                                                body: '' } }
+    end
+
+    assert_template 'articles/new'
+    assert_response :success
+
+    assert_select 'div#error_explanation'
+    assert_select 'div.field_with_errors'
+
+    assert_no_difference 'Article.count', 0 do
+      post articles_path, params: { article: { title: 'title',
+                                                body: '' } }
+    end
+
+    assert_template 'articles/new'
+    assert_response :success
+
+    assert_select 'div#error_explanation'
+    assert_select 'div.field_with_errors'
+
+    assert_no_difference 'Article.count', 0 do
+      post articles_path, params: { article: { title: '',
+                                                body: 'body' } }
+    end
+
+    assert_template 'articles/new'
+    assert_response :success
+
+    assert_select 'div#error_explanation'
+    assert_select 'div.field_with_errors'
+
+    assert_no_difference 'Article.count', 0 do
+      post articles_path, params: { article: { title: 'title',
+                                                body: 'a' * 1000 } }
+    end
+
+    assert_template 'articles/new'
+    assert_response :success
+
+    assert_select 'div#error_explanation'
+    assert_select 'div.field_with_errors'
+  end
+end
