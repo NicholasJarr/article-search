0f27fd35ca5b5b114c17b0e0f075d4e136374dc6
Nicholas Jarretta <nicholas@jarretta.com>
2021-10-22 18:26:37 -0300

    fix prod db bug
    unscope the ip address scope so that no query errors happens in prod

--- app/models/search.rb
+++ app/models/search.rb
@@ -6,5 +6,5 @@ class Search < ApplicationRecord
     }
 
     default_scope { order created_at: :desc }
-    scope :ip_addresses, -> { select(:ip_address).distinct }
+    scope :ip_addresses, -> { unscoped.select(:ip_address).distinct }
 end
