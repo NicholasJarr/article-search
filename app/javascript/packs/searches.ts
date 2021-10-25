5ec09b07c47a4ea7c30af9093957a2eb52b1acd0
Nicholas Jarretta <nicholas@jarretta.com>
2021-10-25 11:10:07 -0300

    add ts code to autosubmit filter changes

---
+++ app/javascript/packs/searches.ts
@@ -0,0 +1,14 @@
+function ready(cb: () => void) {
+    if (document.readyState !== 'loading') {
+        cb()
+    } else {
+        document.addEventListener('DOMContentLoaded', cb)
+    }
+}
+
+ready(() => {
+    const filterForm = $('#table_filter').get(0)
+    const filterSelect = $('#table_filter form select').get(0)
+
+    $(filterSelect).on('change', () => $(filterForm).trigger('submit'))
+})
