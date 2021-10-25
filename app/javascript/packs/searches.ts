11852f953c6a8d4807033103a18460fb51b167d1
Nicholas Jarretta <nicholas@jarretta.com>
2021-10-25 11:20:57 -0300

    fix selector for form

--- app/javascript/packs/searches.ts
+++ app/javascript/packs/searches.ts
@@ -9,7 +9,7 @@ function ready(cb: () => void) {
 }
 
 ready(() => {
-    const filterForm = $('#table_filter').get(0)
+    const filterForm = $('#table_filter form').get(0)
     const filterSelect = $('#table_filter form select').get(0)
 
     $(filterSelect).on('change', () => $(filterForm).trigger('submit'))
